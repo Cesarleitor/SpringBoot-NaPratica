bf5525ba4b90c0e6ce27d7720d277f992aa84c5b
Cesar Augusto <cesar38dev@gmail.com>
2026-03-24 00:00:10 -0300

    teste

---
+++ src/test/java/br/com/cesar/startupTests.java
@@ -0,0 +1,13 @@
+package br.com.cesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class startupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
